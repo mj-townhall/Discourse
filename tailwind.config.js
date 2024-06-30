65782a34659ec261aeb13f802667f23af927b931
Mehul Jain <mehul@townhall.co>
2024-06-30 19:13:15 +0530

    minor changes in color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#007BFF',
+        'custom-gray': {
+          100: '#F8F9FA',
+          500: '#6C757D',
+          900: '#343A40',
+        },
+        'dark-blue' :'#011e29'
+      },
+    },
   },
   plugins: [],
 }
+
+
 
